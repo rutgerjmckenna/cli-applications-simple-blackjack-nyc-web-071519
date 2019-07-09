5064156b3d8a27d4450d01b54248780ce8bbfb62
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-09 01:19:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,6 +53,13 @@ end
 #####################################################
 
 def runner
-  
+   welcome #introduce the game
+  sleep 0.75
+  card_total = initial_round currently have
+  until card_total > 21 #continue game until user loses
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total) #prints out once user has lost
 end
     
